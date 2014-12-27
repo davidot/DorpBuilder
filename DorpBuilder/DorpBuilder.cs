71d45785b91e48c3df7c608db2ad764dceeb5953
davidot <david.t@glazenkamp.net>
2014-12-27 12:13:41 +0100

    Initial commit
    Addind defualt files

---
+++ DorpBuilder/DorpBuilder.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace DorpBuilder
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class DorpBuilder : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        Texture2D image;
+        SpriteFont font;
+        int x = 0;
+
+        public DorpBuilder()
+            : base()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            this.IsMouseVisible = true;
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+
+
+            image =  Content.Load<Texture2D>("image/cityhall");
+            font = Content.Load<SpriteFont>("DorpBuilderFont");
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                Exit();
+
+            KeyboardState keyState = Keyboard.GetState();
+            MouseState mouseState = Mouse.GetState();
+
+            if (keyState.IsKeyDown(Keys.Right))
+            {
+                x += 4;
+            }
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            
+            spriteBatch.Begin();
+
+
+
+            spriteBatch.Draw(image, new Vector2(0, 0), Color.White);
+            spriteBatch.DrawString(font, "Hello", new Vector2(x, 150), Color.Black);
+
+
+            spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ DorpBuilder/Level/Level.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DorpBuilder
+{
+    class Level
+    {
+
+        GroundType[,] ground;
+
+        public Level(int width, int height)
+        {
+            
+        }
+
+    }
+}
